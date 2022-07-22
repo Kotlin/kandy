e9bdc67207b0e7d547f63a91c00c6a41eab0733f
Andrei <kisandy@yandex.ru>
2022-07-22 23:13:06 +0300

    fix df.create

--- ggdsl-dataframe/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/dataframe/plotCreation.kt
+++ ggdsl-dataframe/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/dataframe/plotCreation.kt
@@ -3,13 +3,14 @@ package org.jetbrains.kotlinx.ggdsl.dataframe
 import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.ggdsl.dsl.*
 import org.jetbrains.kotlinx.ggdsl.ir.Plot
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 
 fun DataFrame<*>.plot(block: PlotContext.() -> Unit): Plot {
     return plot(toNamedData(), block)
 }
 
-fun DataFrame<*>.create(block: DataFrame<*>.() -> Plot): Plot {
-    return with(this) {
-        block()
-    }
+inline fun <T> DataFrame<T>.create(block: DataFrame<T>.() -> Plot): Plot {
+    return block()
 }
