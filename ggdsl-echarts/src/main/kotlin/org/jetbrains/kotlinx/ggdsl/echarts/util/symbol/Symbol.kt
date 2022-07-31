de2f60a91977490a00e4c3f4b739983070964899
Andrei <kisandy@yandex.ru>
2022-07-31 16:26:21 +0300

    fix Echarts symbol

--- ggdsl-echarts/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/echarts/util/symbol/Symbol.kt
+++ ggdsl-echarts/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/echarts/util/symbol/Symbol.kt
@@ -2,6 +2,9 @@ package org.jetbrains.kotlinx.ggdsl.echarts.util.symbol
 
 data class Symbol internal constructor(val name: String) {
     companion object {
+        val CIRCLE = Symbol("circle")
+        val TRIANGLE = Symbol("triangle")
+        val RECT = Symbol("rect")
         val DIAMOND = Symbol("diamond")
         val ROUND_RECT = Symbol("roundRect")
         val PIN = Symbol("pin")
