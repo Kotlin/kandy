13e79fecb0c5448dff063ebe2adcb0088f19149a
Andrei <kisandy@yandex.ru>
2022-08-11 15:41:00 +0400

    exclude lp

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "ggdsl"
 include("ggdsl-api")
-include("ggdsl-lets-plot")
+//include("ggdsl-lets-plot")
 include("ggdsl-echarts")
 include("ggdsl-dataframe")
 //include("ggdsl-dataframe-lets-plot")
