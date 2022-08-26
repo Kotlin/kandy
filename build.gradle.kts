d458b44df8ffa53985e337065bab4aed322152c8
Andrei <kisandy@yandex.ru>
2022-08-26 17:10:49 +0400

    fix integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     id("org.jetbrains.dokka") version "1.7.10"
 }
 
-val ggdslVersion = "0.1.4-dev-14-debug-2"
+val ggdslVersion = "0.1.4-dev-15"
 
 allprojects {
     repositories {
--- ggdsl-dataframe-lets-plot/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/dataframe/letsplot/Integration.kt
+++ ggdsl-dataframe-lets-plot/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/dataframe/letsplot/Integration.kt
@@ -9,7 +9,6 @@ internal class Integration : JupyterIntegration() {
     override fun Builder.onLoaded() {
         import("org.jetbrains.kotlinx.ggdsl.dataframe.letsplot.*")
         import("org.jetbrains.kotlinx.ggdsl.dataframe.letsplot.layers.*")
-        println("df-lp")
     }
 
 }
