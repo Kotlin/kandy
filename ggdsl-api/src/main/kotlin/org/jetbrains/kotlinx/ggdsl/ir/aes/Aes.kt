c7bfae84ebdd269b304c142b88af43c2ff354127
devcrocod <devcrocod@gmail.com>
2022-09-12 12:35:22 +0200

    add license

--- ggdsl-api/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/ir/aes/Aes.kt
+++ ggdsl-api/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/ir/aes/Aes.kt
@@ -1,3 +1,7 @@
+/*
+* Copyright 2020-2022 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+*/
+
 package org.jetbrains.kotlinx.ggdsl.ir.aes
 
 import org.jetbrains.kotlinx.ggdsl.dsl.BindingContext
--- ggdsl-dataframe/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/dataframe/mapping.kt
+++ ggdsl-dataframe/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/dataframe/mapping.kt
@@ -1,3 +1,7 @@
+/*
+* Copyright 2020-2022 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+*/
+
 package org.jetbrains.kotlinx.ggdsl.dataframe
 
 import org.jetbrains.kotlinx.dataframe.columns.ColumnReference
