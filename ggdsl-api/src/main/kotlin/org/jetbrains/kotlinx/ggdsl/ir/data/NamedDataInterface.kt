bfb795b54475cf864b87e18706385139d210bbd6
Andrei <kisandy@yandex.ru>
2022-10-10 18:37:13 +0400

    init gather

--- ggdsl-api/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/ir/data/NamedDataInterface.kt
+++ ggdsl-api/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/ir/data/NamedDataInterface.kt
@@ -4,4 +4,10 @@ public interface NamedDataInterface : TableData {
     public val map: Map<String, List<Any>>
 
     public fun groupBy(vararg columnPointers: ColumnPointer<*>): LazyGroupedDataInterface
+
+    public fun gather(
+        vararg columnPointers: ColumnPointer<*>,
+        valuesColumns: String,
+        keysColumns: String
+    ): NamedDataInterface
 }
