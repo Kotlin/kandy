3a5ffc529e319185a91929e603574b3cadb09acb
devcrocod <devcrocod@gmail.com>
2022-12-05 00:14:07 +0100

    update new colors to echarts

--- ggdsl-echarts/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/echarts/settings/Color.kt
+++ ggdsl-echarts/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/echarts/settings/Color.kt
@@ -1,10 +1,9 @@
 package org.jetbrains.kotlinx.ggdsl.echarts.settings
 
 import org.jetbrains.kotlinx.ggdsl.util.color.Color
+import org.jetbrains.kotlinx.ggdsl.util.color.StandardColor
 
 internal fun Color.toHexString(): String = when (this) {
-    is Color.Hex -> this.hex
-    is Color.RGB -> this.toHex().hex
-    is Color.RGBA -> this.toHex().hex
-    is Color.Named -> this.name
+    is StandardColor -> this.description
+    else -> throw Exception("colors mismatch")
 }
