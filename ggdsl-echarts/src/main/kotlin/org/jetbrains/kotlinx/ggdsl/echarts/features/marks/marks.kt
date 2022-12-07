9487944e759642ccb2b546ed83e9238825a29f56
devcrocod <devcrocod@gmail.com>
2022-12-07 18:40:51 +0100

    add marks to area and bar

--- ggdsl-echarts/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/echarts/features/marks/marks.kt
+++ ggdsl-echarts/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/echarts/features/marks/marks.kt
@@ -1,5 +1,7 @@
 package org.jetbrains.kotlinx.ggdsl.echarts.features.marks
 
+import org.jetbrains.kotlinx.ggdsl.echarts.layers.AreaContextImmutable
+import org.jetbrains.kotlinx.ggdsl.echarts.layers.BarContextImmutable
 import org.jetbrains.kotlinx.ggdsl.echarts.layers.LineContextImmutable
 
 public fun LineContextImmutable.markPoint(block: MarkPointFeature.() -> Unit) {
@@ -7,5 +9,21 @@ public fun LineContextImmutable.markPoint(block: MarkPointFeature.() -> Unit) {
 }
 
 public fun LineContextImmutable.markLine(block: MarkLineFeature.() -> Unit) {
+    features[MarkLineFeature.FEATURE_NAME] = MarkLineFeature().apply(block)
+}
+
+public fun AreaContextImmutable.markPoint(block: MarkPointFeature.() -> Unit) {
+    features[MarkPointFeature.FEATURE_NAME] = MarkPointFeature().apply(block)
+}
+
+public fun AreaContextImmutable.markLine(block: MarkLineFeature.() -> Unit) {
+    features[MarkLineFeature.FEATURE_NAME] = MarkLineFeature().apply(block)
+}
+
+public fun BarContextImmutable.markPoint(block: MarkPointFeature.() -> Unit) {
+    features[MarkPointFeature.FEATURE_NAME] = MarkPointFeature().apply(block)
+}
+
+public fun BarContextImmutable.markLine(block: MarkLineFeature.() -> Unit) {
     features[MarkLineFeature.FEATURE_NAME] = MarkLineFeature().apply(block)
 }
