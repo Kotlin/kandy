1b10d4f93c068cb8309ebdf881d925dbb058f98c
devcrocod <devcrocod@gmail.com>
2022-12-07 18:44:52 +0100

    set default size for Symbol

--- ggdsl-echarts/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/echarts/settings/Symbol.kt
+++ ggdsl-echarts/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/echarts/settings/Symbol.kt
@@ -23,36 +23,36 @@ public class Symbol private constructor(internal val name: String) {
 
     public companion object {
         public val EMPTY_CIRCLE: Symbol = Symbol("emptyCircle")
-        public fun emptyCircle(size: Double, rotate: Int?): Symbol = Symbol("emptyCircle", size, rotate)
+        public fun emptyCircle(size: Double, rotate: Int? = null): Symbol = Symbol("emptyCircle", size, rotate)
 
         public val CIRCLE: Symbol = Symbol("circle")
-        public fun circle(size: Double, rotate: Int?): Symbol = Symbol("circle", size, rotate)
+        public fun circle(size: Double, rotate: Int? = null): Symbol = Symbol("circle", size, rotate)
 
         public val TRIANGLE: Symbol = Symbol("triangle")
-        public fun triangle(size: Double, rotate: Int?): Symbol = Symbol("triangle", size, rotate)
+        public fun triangle(size: Double, rotate: Int? = null): Symbol = Symbol("triangle", size, rotate)
 
         public val RECT: Symbol = Symbol("rect")
-        public fun rect(size: Double, rotate: Int?): Symbol = Symbol("rect", size, rotate)
+        public fun rect(size: Double, rotate: Int? = null): Symbol = Symbol("rect", size, rotate)
 
         public val DIAMOND: Symbol = Symbol("diamond")
-        public fun diamond(size: Double, rotate: Int?): Symbol = Symbol("diamond", size, rotate)
+        public fun diamond(size: Double, rotate: Int? = null): Symbol = Symbol("diamond", size, rotate)
 
         public val ROUND_RECT: Symbol = Symbol("roundRect")
-        public fun roundRect(size: Double, rotate: Int?): Symbol = Symbol("roundRect", size, rotate)
+        public fun roundRect(size: Double, rotate: Int? = null): Symbol = Symbol("roundRect", size, rotate)
 
         public val PIN: Symbol = Symbol("pin")
-        public fun pin(size: Double, rotate: Int?): Symbol = Symbol("pin", size, rotate)
+        public fun pin(size: Double, rotate: Int? = null): Symbol = Symbol("pin", size, rotate)
 
         public val ARROW: Symbol = Symbol("arrow")
-        public fun arrow(size: Double, rotate: Int?): Symbol = Symbol("arrow", size, rotate)
+        public fun arrow(size: Double, rotate: Int? = null): Symbol = Symbol("arrow", size, rotate)
 
-        public fun fromUrl(url: String, size: Double?, rotate: Int?): Symbol = Symbol("image://$url", size, rotate)
+        public fun fromUrl(url: String, size: Double? = null, rotate: Int? = null): Symbol = Symbol("image://$url", size, rotate)
 
-        public fun fromUrl(url: URL, size: Double?, rotate: Int?): Symbol = Symbol("image://$url", size, rotate)
+        public fun fromUrl(url: URL, size: Double? = null, rotate: Int? = null): Symbol = Symbol("image://$url", size, rotate)
 
-        public fun fromUrl(url: URI, size: Double?, rotate: Int?): Symbol = Symbol("image://$url", size, rotate)
+        public fun fromUrl(url: URI, size: Double? = null, rotate: Int? = null): Symbol = Symbol("image://$url", size, rotate)
 
-        public fun fromPath(path: String, size: Double?, rotate: Int?): Symbol = Symbol("path://$path", size, rotate)
+        public fun fromPath(path: String, size: Double? = null, rotate: Int? = null): Symbol = Symbol("path://$path", size, rotate)
     }
 
     public fun set(size: Double, rotate: Int? = null): Symbol {
