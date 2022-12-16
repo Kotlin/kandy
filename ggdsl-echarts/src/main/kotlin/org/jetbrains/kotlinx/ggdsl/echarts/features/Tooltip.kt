f58e2074863ff2eb76cd90e4297d777e8898ac43
devcrocod <devcrocod@gmail.com>
2022-12-16 15:11:51 +0100

    add kdocs for tooltip

--- ggdsl-echarts/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/echarts/features/Tooltip.kt
+++ ggdsl-echarts/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/echarts/features/Tooltip.kt
@@ -8,14 +8,51 @@ import org.jetbrains.kotlinx.ggdsl.dsl.internal.PlotDslMarker
 import org.jetbrains.kotlinx.ggdsl.echarts.layers.EChartsLayout
 import org.jetbrains.kotlinx.ggdsl.echarts.translator.option.series.settings.EchartsTooltip
 
+/**
+ * Configures global tooltip.
+ *
+ * - [trigger][Tooltip.trigger] - [type][Trigger] of triggering
+ * - [formatter][Tooltip.formatter] - the content formatter of tooltip's floating layer
+ *
+ * ```kotlin
+ * plot {
+ *  layout {
+ *      tooltip {
+ *          trigger = Trigger.AXIS
+ *          formatter = "layer {a}}
+ *      }
+ *  }
+ * }
+ * ```
+ *
+ * @see org.jetbrains.kotlinx.ggdsl.echarts.layers.layout
+ * @see EChartsLayout
+ */
 public inline fun EChartsLayout.tooltip(crossinline block: Tooltip.() -> Unit) {
     this.tooltip = Tooltip().apply(block)
 }
 
+/**
+ * Type of triggering.
+ *
+ * @property ITEM triggered by data item,
+ * which is mainly used for charts that don't have a category axis like scatter charts or pie charts.
+ * @property AXIS triggered by axes, which is mainly used for charts that have category axes, like bar charts or line charts.
+ * @property NONE trigger nothing.
+ *
+ */
 public enum class Trigger(public val type: String) {
     ITEM("item"), AXIS("axis"), NONE("none")
 }
 
+/**
+ * Tooltip settings.
+ *
+ * @property trigger [type][Trigger] of triggering
+ * @property formatter the content formatter of tooltip's floating layer
+ *
+ * @see Trigger
+ */
 @PlotDslMarker
 public class Tooltip(
     public var trigger: Trigger? = null,
