f4bfbc325247d1f99dd33e025efb6978a0359ac6
devcrocod <devcrocod@gmail.com>
2023-01-25 13:15:24 +0100

    change version to 0.3.0 and add dev versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,8 @@ plugins {
     id("org.jetbrains.dokka") version dokka_version
 }
 
-val ggdslVersion = "0.3.0-rc-2"
+val buildNumber: String? = properties["build_counter"]?.toString()
+val ggdsl_version = version.toString() + if (buildNumber == null) "" else "-dev-$buildNumber"
 
 allprojects {
     repositories {
@@ -32,7 +33,7 @@ allprojects {
     }
 
     group = "org.jetbrains.kotlinx"
-    version = ggdslVersion
+    version = ggdsl_version
     apply(plugin = "kotlin")
     apply(plugin = "org.jetbrains.dokka")
 
