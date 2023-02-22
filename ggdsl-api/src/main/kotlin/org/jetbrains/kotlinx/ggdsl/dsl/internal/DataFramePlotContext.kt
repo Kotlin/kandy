beee23c7ba94305ff30f21301cf4d1404c6273b5
devcrocod <devcrocod@gmail.com>
2023-02-22 12:31:26 +0100

    use delegate pattern for DataFramePlotContext

--- ggdsl-api/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/dsl/internal/DataFramePlotContext.kt
+++ ggdsl-api/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/dsl/internal/DataFramePlotContext.kt
@@ -1,8 +1,7 @@
 package org.jetbrains.kotlinx.ggdsl.dsl.internal
 
 import org.jetbrains.kotlinx.dataframe.*
-import org.jetbrains.kotlinx.dataframe.columns.ColumnPath
-import org.jetbrains.kotlinx.dataframe.columns.ColumnReference
+import org.jetbrains.kotlinx.dataframe.api.getColumns
 import org.jetbrains.kotlinx.ggdsl.ir.Layer
 import org.jetbrains.kotlinx.ggdsl.ir.data.NamedData
 import org.jetbrains.kotlinx.ggdsl.ir.feature.FeatureName
@@ -10,55 +9,12 @@ import org.jetbrains.kotlinx.ggdsl.ir.feature.PlotFeature
 
 public class DataFramePlotContext<T>(
     private val dataFrame: DataFrame<T>
-) : NamedDataPlotContextInterface, LayerCollectorContextImmutable, ColumnsContainer<T> {
+) : NamedDataPlotContextInterface, LayerCollectorContextImmutable, ColumnsContainer<T> by dataFrame {
     override val bindingCollector: BindingCollector = BindingCollector()
     override val layers: MutableList<Layer> = mutableListOf()
     override val data: NamedData = NamedData(dataFrame)
     override val features: MutableMap<FeatureName, PlotFeature> = mutableMapOf()
 
-
-    override fun columns(): List<AnyCol> {
-        return dataFrame.columns()
-    }
-
-    override fun columnsCount(): Int {
-        return dataFrame.columnsCount()
-    }
-
-    override fun containsColumn(name: String): Boolean {
-        return dataFrame.containsColumn(name)
-    }
-
-    override fun containsColumn(path: ColumnPath): Boolean {
-        return dataFrame.containsColumn(path)
-    }
-
-    override fun <C> get(columns: ColumnsSelector<T, C>): List<DataColumn<C>> {
-        return dataFrame[columns]
-    }
-
-    override fun getColumnIndex(name: String): Int {
-        return dataFrame.getColumnIndex(name)
-    }
-
-    override fun getColumnOrNull(index: Int): AnyCol? {
-        return dataFrame.getColumnOrNull(index)
-    }
-
-    override fun getColumnOrNull(name: String): AnyCol? {
-        return dataFrame.getColumnOrNull(name)
-    }
-
-    override fun getColumnOrNull(path: ColumnPath): AnyCol? {
-        return dataFrame.getColumnOrNull(path)
-    }
-
-    override fun <R> getColumnOrNull(column: ColumnReference<R>): DataColumn<R>? {
-        return dataFrame.getColumnOrNull(column)
-    }
-
-    override fun <R> getColumnOrNull(column: ColumnSelector<T, R>): DataColumn<R>? {
-        return dataFrame.getColumnOrNull(column)
-    }
-
+    public fun <C> columns(selector: ColumnsSelector<T, C>): List<DataColumn<C>> = dataFrame.get(selector)
+    public fun <C> columns(vararg columns: String): List<AnyCol> = dataFrame.getColumns(*columns)
 }
