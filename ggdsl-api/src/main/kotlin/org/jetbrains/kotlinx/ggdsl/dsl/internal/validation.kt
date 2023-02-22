c8594319aa5d03b190058dd093ef29152f56898e
devcrocod <devcrocod@gmail.com>
2023-02-22 16:04:07 +0100

    fix validate

--- ggdsl-api/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/dsl/internal/validation.kt
+++ ggdsl-api/src/main/kotlin/org/jetbrains/kotlinx/ggdsl/dsl/internal/validation.kt
@@ -1,6 +1,8 @@
 package org.jetbrains.kotlinx.ggdsl.dsl.internal
 
-import org.jetbrains.kotlinx.ggdsl.ir.Layer
+import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.kotlinx.dataframe.api.containsColumn
+import org.jetbrains.kotlinx.dataframe.type
 import org.jetbrains.kotlinx.ggdsl.ir.Plot
 import org.jetbrains.kotlinx.ggdsl.ir.aes.AesName
 import org.jetbrains.kotlinx.ggdsl.ir.bindings.Mapping
@@ -10,7 +12,7 @@ import org.jetbrains.kotlinx.ggdsl.ir.bindings.ScaledMapping
 import org.jetbrains.kotlinx.ggdsl.ir.data.GroupedData
 import org.jetbrains.kotlinx.ggdsl.ir.data.NamedData
 import org.jetbrains.kotlinx.ggdsl.ir.data.TableData
-import kotlin.reflect.KType
+
 /*
 /**
  * Checks if all columns of dataset are the same size.
@@ -35,27 +37,21 @@ public fun NamedData.validate() {
  * Checks if all group keys are the column names.
  */
 public fun GroupedData.validate() {
+    val df = origin.dataFrame
     keys.forEach {
-        require(it in origin.dataFrame.columnNames()) {
+        require(df.containsColumn(it)) {
             "$it not is the name of the column of the original dataframe"
         }
     }
 }
 
 /**
- * Validates layers: validates all mappings in a layer.
+ * Returns dataframe from TableData object. If TableData is GroupedData, takes origin dataframe.
  */
-public fun Layer.validate(plotDataset: TableData) {
-    val columns = (dataset ?: plotDataset).columnsWithType()
-    mappings.validate(columns)
-}
-
-internal fun TableData.columnsWithType(): Map<String, KType> {
+internal fun TableData.getDataFrame(): DataFrame<*> {
     return when (this) {
-        is NamedData -> dataFrame.columns().associate {
-            it.name() to it.type()
-        }
-        is GroupedData -> origin.columnsWithType()
+        is NamedData -> dataFrame
+        is GroupedData -> origin.dataFrame
     }
 }
 /* TODO
@@ -71,21 +67,21 @@ internal fun Map<AesName, Mapping>.validateGroups(groupKys: Set<String>) {
 
  */
 
-internal fun Map<AesName, Mapping>.validate(columns: Map<String, KType>) {
-    val columnNames = columns.keys
+// TODO(this validation need only for case when we don't work with df initially)
+internal fun Map<AesName, Mapping>.validate(df: DataFrame<*>) {
     forEach { (_, mapping) ->
-        val columnName = when (mapping) {
-            is ScaledMapping<*> -> mapping.columnScaled.source.name()
-            is NonScalableNonPositionalMapping<*> -> mapping.source.name()
-            is NonScalablePositionalMapping<*> -> mapping.source.name()
+        val column = when (mapping) {
+            is ScaledMapping<*> -> mapping.columnScaled.source
+            is NonScalableNonPositionalMapping<*> -> mapping.source
+            is NonScalablePositionalMapping<*> -> mapping.source
         }
-        require(columnName in columnNames) {
-            "No column with name \"$columnName\" found in dataframe with columns $columnNames"
+        require(df.containsColumn(column)) {
+            "No column with name \"${column.name()}\" found in dataframe with columns ${df.columnNames()}"
         }
-        val expectedType = columns[columnName]
+        val expectedType = df[column].type
         val actualType = mapping.domainType
         require(expectedType == actualType) {
-            "Expected $expectedType as type of the pointer to column \"$columnName\" but actual type is $actualType"
+            "Expected $expectedType as type of the pointer to column \"${column.name()}\" but actual type is $actualType"
         }
     }
 }
@@ -94,9 +90,9 @@ internal fun Map<AesName, Mapping>.validate(columns: Map<String, KType>) {
  * Checks presence of column with a given name.
  */
 public fun TableData.validateColumn(columnName: String) {
-    val columns = columnsWithType()
-    require(columnName in columns) {
-        "No column with name \"$columnName\" found in dataframe with columns ${columns.keys}"
+    val df = getDataFrame()
+    require(df.containsColumn(columnName)) {
+        "No column with name \"$columnName\" found in dataframe with columns ${df.columnNames()}"
     }
 }
 
@@ -104,8 +100,9 @@ public fun TableData.validateColumn(columnName: String) {
  * Validates plot: validates all global mappings and layers.
  */
 public fun Plot.validate() {
-    globalMappings.validate(dataset.columnsWithType())
+    globalMappings.validate(dataset.getDataFrame())
     layers.forEach {
-        it.validate(dataset)
+        // validate all mappings in each layer with layer or plot data
+        it.mappings.validate((it.dataset ?: dataset).getDataFrame())
     }
 }
