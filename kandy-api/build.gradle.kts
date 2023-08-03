5fd8d32fe02843d8e6540af11c4ad8dc9f9a568d
devcrocod <devcrocod@gmail.com>
2023-08-03 18:02:35 +0200

    Add MockK dependency to kandy-api module

--- kandy-api/build.gradle.kts
+++ kandy-api/build.gradle.kts
@@ -11,10 +11,12 @@ repositories {
 //val serialization_version: String by project
 val datetime_version: String by project
 val dataframe_version: String by project
+val mockk_version: String by project
 
 dependencies {
     implementation(kotlin("stdlib"))
     testImplementation(kotlin("test"))
+    testImplementation("io.mockk:mockk:${mockk_version}")
     api("org.jetbrains.kotlinx:kotlinx-datetime:$datetime_version")
     api("org.jetbrains.kotlinx:dataframe:$dataframe_version")
    // implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serialization_version")
