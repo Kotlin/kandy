6015f9957e3153bffd1baa19394e95e9cc25f5a1
devcrocod <devcrocod@gmail.com>
2023-08-11 20:09:14 +0200

    Refactor StandardColor interface and its implementations

--- kandy-api/src/main/kotlin/org/jetbrains/kotlinx/kandy/util/color/StandardColor.kt
+++ kandy-api/src/main/kotlin/org/jetbrains/kotlinx/kandy/util/color/StandardColor.kt
@@ -11,7 +11,7 @@ import kotlin.math.roundToInt
  *
  * @property description base single-[String] description.
  */
-public sealed interface StandardColor: Color {
+public sealed interface StandardColor : Color {
     public val description: String
 
     /**
@@ -20,7 +20,7 @@ public sealed interface StandardColor: Color {
      * @property hex [Hex] representation of this color.
      * @property hexString hex-[String] representation of this color.
      */
-    public sealed interface AsHexColor: StandardColor {
+    public sealed interface AsHexColor : StandardColor {
         public val hex: Hex
         public val hexString: String
             get() = hex.hexString
@@ -41,9 +41,15 @@ public sealed interface StandardColor: Color {
      */
     public data class RGB internal constructor(val r: Int, val g: Int, val b: Int) : AsHexColor {
         public fun toRGBA(a: Double = 1.0): RGBA = RGBA(this.copy(), a)
-        public override val hex: Hex = Hex(
-            String.format("#%02X%02X%02X", r, g, b)
-        )
+        public override val hex: Hex by lazy {
+            Hex(String.format("#%02X%02X%02X", r, g, b))
+        }
+
+        init {
+            require(r in 0..255 && g in 0..255 && b in 0..255) {
+                "RGB components must be in the range of 0 to 255. Received: r=$r, g=$g, b=$b"
+            }
+        }
 
         override fun compareTo(other: Color): Int {
             //TODO("Not yet implemented")
@@ -62,12 +68,18 @@ public sealed interface StandardColor: Color {
      */
     public data class RGBA internal constructor(val rgb: RGB, val a: Double) : AsHexColor {
         public fun toRGB(): RGB = rgb.copy()
-        public override val hex: Hex = Hex(
-            buildString {
-                append(rgb.hexString)
-                append(((a * 255).roundToInt() or (1 shr 8)).toString(16))
-            }
-        )
+        public override val hex: Hex by lazy {
+            Hex(
+                buildString {
+                    append(rgb.hexString)
+                    append(((a * 255).roundToInt() or (1 shr 8)).toString(16))
+                }
+            )
+        }
+
+        init {
+            require(a in 0.0..1.0) { "Alpha component must be in the range of 0.0 to 1.0. Received: a=$a" }
+        }
 
         override fun compareTo(other: Color): Int {
             //TODO("Not yet implemented")
@@ -85,6 +97,12 @@ public sealed interface StandardColor: Color {
         override val description: String
             get() = name
 
+        init {
+            require(name.isNotEmpty()) {
+                "Name property in `StandardColor.Named` must not be empty. Please provide a valid color name."
+            }
+        }
+
         override fun compareTo(other: Color): Int {
             //TODO("Not yet implemented")
             return 1
@@ -98,6 +116,13 @@ public sealed interface StandardColor: Color {
      */
     public data class Hex internal constructor(override val hexString: String) : AsHexColor {
         override val hex: Hex = this
+
+        init {
+            require(hexString.matches("^#([a-fA-F0-9]{6}|[a-fA-F0-9]{8})$".toRegex())) {
+                "Invalid hex string format: '$hexString'. A valid hex string must start with '#' followed by either 6 or 8 hexadecimal characters."
+            }
+        }
+
         override fun compareTo(other: Color): Int {
             //TODO("Not yet implemented")
             return 1
