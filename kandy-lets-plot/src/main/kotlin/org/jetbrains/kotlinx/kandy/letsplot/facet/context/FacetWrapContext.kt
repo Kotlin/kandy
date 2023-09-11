28a61c1aee3aed6ab1dc9b592c37addb5d71038f
devcrocod <devcrocod@gmail.com>
2023-09-11 18:36:52 +0200

    Update kdocs for FacetWrapContext in kandy-lets-plot

--- kandy-lets-plot/src/main/kotlin/org/jetbrains/kotlinx/kandy/letsplot/facet/context/FacetWrapContext.kt
+++ kandy-lets-plot/src/main/kotlin/org/jetbrains/kotlinx/kandy/letsplot/facet/context/FacetWrapContext.kt
@@ -15,7 +15,9 @@ import org.jetbrains.kotlinx.kandy.letsplot.facet.facetWrap
 import org.jetbrains.kotlinx.kandy.letsplot.facet.feature.FacetWrapFeature
 
 /**
- * Context opened by [LayerPlotContext.facetWrap]. Collects facets variables.
+ * Represents the context for configuring and adding facets to a plot within the [LayerPlotContext].
+ * This context is initiated by invoking [LayerPlotContext.facetWrap],
+ * which collects variables that are used to create facet plots.
  */
 public class FacetWrapContext @PublishedApi internal constructor() {
     private val facets = mutableListOf<ColumnReference<*>>()
@@ -38,10 +40,29 @@ public class FacetWrapContext @PublishedApi internal constructor() {
      * "Score: {.2f}" -> "Score: 12.45"
      * "'Score: {}' "-> "Score: 12.454789"
      */
+    /**
+     * Adds a new facet to the plot, defined by the specified variable.
+     * This method allows the division of data into different subsets,
+     * where each subset is represented as a separate panel in the plot.
+     *
+     * @param variable a reference to the column that defines the variable for this facet.
+     * The values in this column are used to create different subsets of the data, which are then plotted as separate panels.
+     * @param order specifies the ordering direction of the values in this facet.
+     * It can be either [OrderDirection.ASCENDING] or [OrderDirection.DESCENDING].
+     * The default value is [OrderDirection.ASCENDING].
+     * @param format a string that specifies the format pattern for displaying the values in this facet in rows.
+     * This can either be a simple number format (like "d")
+     * or a string template where the number format is surrounded by curly braces (for example, "{d} cylinders").
+     * > That the "$" character must be escaped as "\$".
+     *
+     *
+     * Examples of format patterns:
+     * - ".2f" -> "12.45"
+     * - "Score: {.2f}" -> "Score: 12.45"
+     * - "'Score: {}'" -> "Score: 12.454789"
+     */
     public fun facet(
-        variable: ColumnReference<*>,
-        order: OrderDirection = OrderDirection.ASCENDING,
-        format: String? = null
+        variable: ColumnReference<*>, order: OrderDirection = OrderDirection.ASCENDING, format: String? = null
     ) {
         facets.add(variable)
         orders.add(order)
@@ -49,11 +70,8 @@ public class FacetWrapContext @PublishedApi internal constructor() {
     }
 
     internal fun toFeature(
-        datasetHandler: DatasetHandler,
-        nCol: Int?,
-        nRow: Int?,
-        scalesSharing: ScalesSharing,
-        direction: Direction,
+        datasetHandler: DatasetHandler, nCol: Int?, nRow: Int?,
+        scalesSharing: ScalesSharing, direction: Direction,
     ) =
         FacetWrapFeature(
             facets.map { datasetHandler.takeColumn(it.name()) },
