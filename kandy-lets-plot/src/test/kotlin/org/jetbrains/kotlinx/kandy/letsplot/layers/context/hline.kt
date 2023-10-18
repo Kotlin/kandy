9ff4a6e4b43b7e9b2acc617b8c4077d34f59d453
devcrocod <devcrocod@gmail.com>
2023-10-18 11:52:14 +0200

    Add tests for hline

--- kandy-lets-plot/src/test/kotlin/org/jetbrains/kotlinx/kandy/letsplot/layers/context/hline.kt
+++ kandy-lets-plot/src/test/kotlin/org/jetbrains/kotlinx/kandy/letsplot/layers/context/hline.kt
@@ -10,14 +10,12 @@ import org.jetbrains.kotlinx.kandy.ir.bindings.PositionalMapping
 import org.jetbrains.kotlinx.kandy.ir.bindings.PositionalSetting
 import org.jetbrains.kotlinx.kandy.letsplot.internal.ALPHA
 import org.jetbrains.kotlinx.kandy.letsplot.internal.COLOR
-import org.jetbrains.kotlinx.kandy.letsplot.internal.INTERCEPT
 import org.jetbrains.kotlinx.kandy.letsplot.internal.LINE_TYPE
 import org.jetbrains.kotlinx.kandy.letsplot.internal.LetsPlotPositionalMappingParameters
 import org.jetbrains.kotlinx.kandy.letsplot.internal.SIZE
-import org.jetbrains.kotlinx.kandy.letsplot.internal.SLOPE
 import org.jetbrains.kotlinx.kandy.letsplot.internal.X
-import org.jetbrains.kotlinx.kandy.letsplot.internal.Y
-import org.jetbrains.kotlinx.kandy.letsplot.layers.geom.AB_LINE
+import org.jetbrains.kotlinx.kandy.letsplot.internal.Y_INTERCEPT
+import org.jetbrains.kotlinx.kandy.letsplot.layers.geom.H_LINE
 import org.jetbrains.kotlinx.kandy.letsplot.settings.LineType
 import org.jetbrains.kotlinx.kandy.util.color.Color
 import org.jetbrains.kotlinx.kandy.util.context.invoke
@@ -26,191 +24,136 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
-class ABLineTests {
+class HLineTests {
+    private val yIntercept = listOf("first").toColumn("yIntercept")
     private val type = listOf("dot").toColumn("type")
     private val color = listOf("blue").toColumn("color")
     private val alpha = listOf(0.5).toColumn("alpha")
     private val width = listOf(0.9).toColumn("width")
-    private val slope = listOf(2).toColumn("slope")
-    private val intercept = listOf(0).toColumn("intercept")
 
-    private val df = dataFrameOf(type, color, alpha, width, slope, intercept)
+    private val df = dataFrameOf(yIntercept, type, color, alpha, width)
 
     private val parentContext = DataFramePlotContext(df)
-    private lateinit var context: ABLineContext
+    private lateinit var context: HLineContext
 
     @BeforeTest
     fun setUp() {
-        context = ABLineContext(parentContext)
+        context = HLineContext(parentContext)
     }
 
     @Test
-    fun `geom is AB_LINE`() {
-        assertEquals(AB_LINE, context.geom)
+    fun `geom is H_LINE`() {
+        assertEquals(H_LINE, context.geom)
     }
 
     @Test
-    fun `requiredAes contains SLOPE and INTERCEPT`() {
-        assertTrue(context.requiredAes.contains(SLOPE))
-        assertTrue(context.requiredAes.contains(INTERCEPT))
+    fun `requiredAes contains Y_INTERCEPT`() {
+        assertTrue(context.requiredAes.contains(Y_INTERCEPT))
     }
 
     @Test
-    fun `color const for abline`() {
+    fun `color const for hline`() {
         context.color = Color.BLUE
         assertEquals(Color.BLUE, (context.bindingCollector.settings[COLOR] as NonPositionalSetting<*>).value)
     }
 
     @Test
-    fun `color str mapping for abline`() {
+    fun `color str mapping for hline`() {
         context.color("color")
         assertEquals("color", (context.bindingCollector.mappings[COLOR] as NonPositionalMapping<*, *>).columnID)
     }
 
     @Test
-    fun `color dataColumn mapping for abline`() {
+    fun `color dataColumn mapping for hline`() {
         context.color(color)
         assertEquals("color", (context.bindingCollector.mappings[COLOR] as NonPositionalMapping<*, *>).columnID)
     }
 
     @Test
-    fun `color iterable mapping for abline`() {
+    fun `color iterable mapping for hline`() {
         context.color(listOf("red"))
         assertEquals("color", (context.bindingCollector.mappings[COLOR] as NonPositionalMapping<*, *>).columnID)
     }
 
     @Test
-    fun `type const for abline`() {
+    fun `type const for hline`() {
         context.type = LineType.DOTDASH
         assertEquals(LineType.DOTDASH, (context.bindingCollector.settings[LINE_TYPE] as NonPositionalSetting<*>).value)
     }
 
     @Test
-    fun `type str mapping for abline`() {
+    fun `type str mapping for hline`() {
         context.type("type")
         assertEquals("type", (context.bindingCollector.mappings[LINE_TYPE] as NonPositionalMapping<*, *>).columnID)
     }
 
     @Test
-    fun `type dataColumn mapping for abline`() {
+    fun `type dataColumn mapping for hline`() {
         context.type(type)
         assertEquals("type", (context.bindingCollector.mappings[LINE_TYPE] as NonPositionalMapping<*, *>).columnID)
     }
 
     @Test
-    fun `type iterable mapping for abline`() {
+    fun `type iterable mapping for hline`() {
         context.type(listOf("dot"))
         assertEquals("linetype", (context.bindingCollector.mappings[LINE_TYPE] as NonPositionalMapping<*, *>).columnID)
     }
 
     @Test
-    fun `alpha const for abline`() {
+    fun `alpha const for hline`() {
         context.alpha = 0.1
         assertEquals(0.1, (context.bindingCollector.settings[ALPHA] as NonPositionalSetting<*>).value)
     }
 
     @Test
-    fun `alpha str mapping for abline`() {
+    fun `alpha str mapping for hline`() {
         context.alpha("alpha")
         assertEquals("alpha", (context.bindingCollector.mappings[ALPHA] as NonPositionalMapping<*, *>).columnID)
     }
 
     @Test
-    fun `alpha dataColumn mapping for abline`() {
+    fun `alpha dataColumn mapping for hline`() {
         context.alpha(alpha)
         assertEquals("alpha", (context.bindingCollector.mappings[ALPHA] as NonPositionalMapping<*, *>).columnID)
     }
 
     @Test
-    fun `alpha iterable mapping for abline`() {
+    fun `alpha iterable mapping for hline`() {
         context.alpha(listOf(0.2, 0.5, .1))
         assertEquals("alpha", (context.bindingCollector.mappings[ALPHA] as NonPositionalMapping<*, *>).columnID)
     }
 
     @Test
-    fun `width const for abline`() {
+    fun `width const for hline`() {
         context.width = .5
         assertEquals(.5, (context.bindingCollector.settings[SIZE] as NonPositionalSetting<*>).value)
     }
 
     @Test
-    fun `width str mapping for abline`() {
+    fun `width str mapping for hline`() {
         context.width("width")
         assertEquals("width", (context.bindingCollector.mappings[SIZE] as NonPositionalMapping<*, *>).columnID)
     }
 
     @Test
-    fun `width dataColumn mapping for abline`() {
+    fun `width dataColumn mapping for hline`() {
         context.width(width)
         assertEquals("width", (context.bindingCollector.mappings[SIZE] as NonPositionalMapping<*, *>).columnID)
     }
 
     @Test
-    fun `width iterable mapping for abline`() {
+    fun `width iterable mapping for hline`() {
         context.width(listOf(0.2, .1))
         assertEquals("size", (context.bindingCollector.mappings[SIZE] as NonPositionalMapping<*, *>).columnID)
     }
 
     @Test
-    fun `slope const for abline`() {
-        context.slope.constant(2)
-        assertEquals(2, (context.bindingCollector.settings[SLOPE] as PositionalSetting<*>).value)
-    }
-
-    @Test
-    fun `slope str mapping for abline`() {
-        context.slope("slope")
-        assertEquals("slope", (context.bindingCollector.mappings[SLOPE] as PositionalMapping<*>).columnID)
-    }
-
-    @Test
-    fun `slope dataColumn mapping for abline`() {
-        context.slope(slope)
-        assertEquals("slope", (context.bindingCollector.mappings[SLOPE] as PositionalMapping<*>).columnID)
-    }
-
-    @Test
-    fun `slope iterable mapping for abline`() {
-        context.slope(listOf(1, 2))
-        assertEquals("slope", (context.bindingCollector.mappings[SLOPE] as PositionalMapping<*>).columnID)
-    }
-
-    @Test
-    fun `intercept const for abline`() {
-        context.intercept.constant(0)
-        assertEquals(0, (context.bindingCollector.settings[INTERCEPT] as PositionalSetting<*>).value)
-    }
-
-    @Test
-    fun `intercept str mapping for abline`() {
-        context.intercept("intercept")
-        assertEquals("intercept", (context.bindingCollector.mappings[INTERCEPT] as PositionalMapping<*>).columnID)
-    }
-
-    @Test
-    fun `intercept dataColumn mapping for abline`() {
-        context.intercept(intercept)
-        assertEquals("intercept", (context.bindingCollector.mappings[INTERCEPT] as PositionalMapping<*>).columnID)
-    }
-
-    @Test
-    fun `intercept iterable mapping for abline`() {
-        context.intercept(listOf(0.2, .1))
-        assertEquals("intercept", (context.bindingCollector.mappings[INTERCEPT] as PositionalMapping<*>).columnID)
-    }
-
-    @Test
-    fun `xy modify for abline`() {
+    fun `x free for hline`() {
         context.x {
             axis {
                 name = "xAxis"
             }
-            scale = continuous(-0.9, 3.1)
-        }
-        context.y {
-            axis {
-                name = "yAxis"
-            }
+            scale = continuous(-3.0, 3.0)
         }
 
         assertEquals(X, context.bindingCollector.freeScales[X]?.aes)
@@ -218,11 +161,19 @@ class ABLineTests {
             "xAxis",
             (context.bindingCollector.freeScales[X]?.parameters as? LetsPlotPositionalMappingParameters<*>)?.axis?.name
         )
+    }
 
-        assertEquals(Y, context.bindingCollector.freeScales[Y]?.aes)
-        assertEquals(
-            "yAxis",
-            (context.bindingCollector.freeScales[Y]?.parameters as? LetsPlotPositionalMappingParameters<*>)?.axis?.name
-        )
+    @Test
+    fun `y intercept const for hline`() {
+        context.yIntercept.constant(5)
+        assertEquals(5, (context.bindingCollector.settings[Y_INTERCEPT] as? PositionalSetting<*>)?.value)
+    }
+
+    @Test
+    fun `y intercept mapping for hline`() {
+        context.yIntercept(yIntercept)
+
+        assertEquals(Y_INTERCEPT, context.bindingCollector.mappings[Y_INTERCEPT]?.aes)
+        assertEquals("yIntercept", (context.bindingCollector.mappings[Y_INTERCEPT] as? PositionalMapping<*>)?.columnID)
     }
 }
