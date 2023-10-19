f34bf1c888391c914a238a9565b80a4c5c903f0b
devcrocod <devcrocod@gmail.com>
2023-10-19 23:06:35 +0200

    Add tests for ribbon

--- kandy-lets-plot/src/test/kotlin/org/jetbrains/kotlinx/kandy/letsplot/layers/context/ribbon.kt
+++ kandy-lets-plot/src/test/kotlin/org/jetbrains/kotlinx/kandy/letsplot/layers/context/ribbon.kt
@@ -2,124 +2,133 @@ package org.jetbrains.kotlinx.kandy.letsplot.layers.context
 
 import org.jetbrains.kotlinx.dataframe.api.dataFrameOf
 import org.jetbrains.kotlinx.dataframe.api.toColumn
+import org.jetbrains.kotlinx.kandy.dsl.continuous
 import org.jetbrains.kotlinx.kandy.dsl.internal.DataFramePlotContext
 import org.jetbrains.kotlinx.kandy.ir.bindings.NonPositionalMapping
 import org.jetbrains.kotlinx.kandy.ir.bindings.NonPositionalSetting
+import org.jetbrains.kotlinx.kandy.ir.bindings.PositionalSetting
+import org.jetbrains.kotlinx.kandy.ir.scale.PositionalContinuousScale
 import org.jetbrains.kotlinx.kandy.letsplot.internal.ALPHA
 import org.jetbrains.kotlinx.kandy.letsplot.internal.FILL
-import org.jetbrains.kotlinx.kandy.letsplot.internal.X_MAX
-import org.jetbrains.kotlinx.kandy.letsplot.internal.X_MIN
+import org.jetbrains.kotlinx.kandy.letsplot.internal.X
 import org.jetbrains.kotlinx.kandy.letsplot.internal.Y_MAX
 import org.jetbrains.kotlinx.kandy.letsplot.internal.Y_MIN
-import org.jetbrains.kotlinx.kandy.letsplot.layers.geom.RECT
+import org.jetbrains.kotlinx.kandy.letsplot.layers.geom.RIBBON
 import org.jetbrains.kotlinx.kandy.util.color.Color
+import org.jetbrains.kotlinx.kandy.util.context.invoke
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
-class RectTests {
-    private val xMin = listOf(0.1).toColumn("xMin")
-    private val xMax = listOf(2.1).toColumn("xMax")
+class RibbonTests {
+    private val xAxis = listOf(0.5).toColumn("xAxis")
     private val yMin = listOf(1).toColumn("yMin")
     private val yMax = listOf(5).toColumn("yMax")
     private val color = listOf("blue").toColumn("color")
     private val alpha = listOf(0.5).toColumn("alpha")
 
-    private val df = dataFrameOf(xMin, xMax, yMin, yMax, color, alpha)
+    private val df = dataFrameOf(xAxis, yMin, yMax, color, alpha)
 
     private val parentContext = DataFramePlotContext(df)
-    private lateinit var context: RectContext
+    private lateinit var context: RibbonContext
 
     @BeforeTest
     fun setUp() {
-        context = RectContext(parentContext)
+        context = RibbonContext(parentContext)
     }
 
     @Test
-    fun `geom is RECT`() {
-        assertEquals(RECT, context.geom)
+    fun `geom is RIBBON`() {
+        assertEquals(RIBBON, context.geom)
     }
 
     @Test
     fun `requiredAes contains X_MIN, X_MAX, Y_MIN, Y_MAX`() {
-        assertTrue(context.requiredAes.contains(X_MIN))
-        assertTrue(context.requiredAes.contains(X_MAX))
+        assertTrue(context.requiredAes.contains(X))
         assertTrue(context.requiredAes.contains(Y_MIN))
         assertTrue(context.requiredAes.contains(Y_MAX))
     }
 
     @Test
-    fun `fillColor const for rect`() {
+    fun `color const for ribbon`() {
         context.fillColor = Color.BLUE
         assertEquals(Color.BLUE, (context.bindingCollector.settings[FILL] as NonPositionalSetting<*>).value)
     }
 
     @Test
-    fun `fillColor str mapping for rect`() {
+    fun `color str mapping for ribbon`() {
         context.fillColor("color")
         assertEquals("color", (context.bindingCollector.mappings[FILL] as NonPositionalMapping<*, *>).columnID)
     }
 
     @Test
-    fun `fillColor dataColumn mapping for rect`() {
+    fun `color dataColumn mapping for ribbon`() {
         context.fillColor(color)
         assertEquals("color", (context.bindingCollector.mappings[FILL] as NonPositionalMapping<*, *>).columnID)
     }
 
     @Test
-    fun `fillColor iterable mapping for rect`() {
+    fun `color iterable mapping for ribbon`() {
         context.fillColor(listOf("red"))
         assertEquals("fill", (context.bindingCollector.mappings[FILL] as NonPositionalMapping<*, *>).columnID)
     }
 
     @Test
-    fun `alpha const for rect`() {
+    fun `alpha const for ribbon`() {
         context.alpha = 0.1
         assertEquals(0.1, (context.bindingCollector.settings[ALPHA] as NonPositionalSetting<*>).value)
     }
 
     @Test
-    fun `alpha str mapping for rect`() {
+    fun `alpha str mapping for ribbon`() {
         context.alpha("alpha")
         assertEquals("alpha", (context.bindingCollector.mappings[ALPHA] as NonPositionalMapping<*, *>).columnID)
     }
 
     @Test
-    fun `alpha dataColumn mapping for rect`() {
+    fun `alpha dataColumn mapping for ribbon`() {
         context.alpha(alpha)
         assertEquals("alpha", (context.bindingCollector.mappings[ALPHA] as NonPositionalMapping<*, *>).columnID)
     }
 
     @Test
-    fun `alpha iterable mapping for rect`() {
+    fun `alpha iterable mapping for ribbon`() {
         context.alpha(listOf(0.2, 0.5, .1))
         assertEquals("alpha", (context.bindingCollector.mappings[ALPHA] as NonPositionalMapping<*, *>).columnID)
     }
 
     @Test
-    fun `xMin for rect`() {
-        context.xMin(xMin)
-        assertEquals(X_MIN, context.bindingCollector.mappings[X_MIN]?.aes)
-        assertEquals("xMin", context.bindingCollector.mappings[X_MIN]?.columnID)
+    fun `x const for ribbon`() {
+        context.x.constant(5.0)
+        assertEquals(X, (context.bindingCollector.settings[X] as PositionalSetting<*>).aes)
+        assertEquals(5.0, (context.bindingCollector.settings[X] as PositionalSetting<*>).value)
     }
 
     @Test
-    fun `xMax for rect`() {
-        context.xMax(xMax)
-        assertEquals(X_MAX, context.bindingCollector.mappings[X_MAX]?.aes)
-        assertEquals("xMax", context.bindingCollector.mappings[X_MAX]?.columnID)
+    fun `x for ribbon`() {
+        context.x(xAxis) {
+            axis {
+                name = "x axis"
+            }
+            scale = continuous(0.1..3.7)
+        }
+
+        assertEquals(X, context.bindingCollector.mappings[X]?.aes)
+        assertEquals("xAxis", context.bindingCollector.mappings[X]?.columnID)
+        assertEquals(.1, (context.bindingCollector.mappings[X]?.parameters?.scale as PositionalContinuousScale<*>).min)
+        assertEquals(3.7, (context.bindingCollector.mappings[X]?.parameters?.scale as PositionalContinuousScale<*>).max)
     }
 
     @Test
-    fun `yMin for rect`() {
+    fun `yMin for ribbon`() {
         context.yMin(yMin)
         assertEquals(Y_MIN, context.bindingCollector.mappings[Y_MIN]?.aes)
         assertEquals("yMin", context.bindingCollector.mappings[Y_MIN]?.columnID)
     }
 
     @Test
-    fun `yMax for rect`() {
+    fun `yMax for ribbon`() {
         context.yMax(yMax)
         assertEquals(Y_MAX, context.bindingCollector.mappings[Y_MAX]?.aes)
         assertEquals("yMax", context.bindingCollector.mappings[Y_MAX]?.columnID)
