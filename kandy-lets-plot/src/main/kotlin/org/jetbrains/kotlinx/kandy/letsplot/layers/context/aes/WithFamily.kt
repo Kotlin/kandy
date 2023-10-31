15e2536c90b103624926e2d58b16705fb61342ae
devcrocod <devcrocod@gmail.com>
2023-10-31 22:40:42 +0100

    Add kdoc for font family

--- kandy-lets-plot/src/main/kotlin/org/jetbrains/kotlinx/kandy/letsplot/layers/context/aes/WithFamily.kt
+++ kandy-lets-plot/src/main/kotlin/org/jetbrains/kotlinx/kandy/letsplot/layers/context/aes/WithFamily.kt
@@ -9,16 +9,34 @@ import org.jetbrains.kotlinx.dataframe.columns.ColumnReference
 import org.jetbrains.kotlinx.kandy.dsl.internal.BindingContext
 import org.jetbrains.kotlinx.kandy.ir.bindings.NonPositionalMapping
 import org.jetbrains.kotlinx.kandy.letsplot.internal.FONT_FAMILY
+import org.jetbrains.kotlinx.kandy.letsplot.settings.font.FontFace
 import org.jetbrains.kotlinx.kandy.letsplot.settings.font.FontFamily
 import kotlin.reflect.KProperty
 
+/**
+ * Interface for configuring the `family` aesthetic, which refers to the font family in text elements of a plot layer.
+ *
+ * This interface provides methods to either directly set the font face or map a data column to it.
+ */
 public interface WithFamily : BindingContext {
+
+    /**
+     * Sets a constant font `family` for text elements in the plot layer.
+     *
+     * @property family the font family to be set, represented as a [FontFamily] object.
+     */
     public var family: FontFamily?
         get() = null
         set(value) {
             addNonPositionalSetting(FONT_FAMILY, value)
         }
 
+    /**
+     * Maps the `family` aesthetic to a data column by [ColumnReference].
+     *
+     * @param column the data column to be mapped.
+     * @return a [NonPositionalMapping] object representing the mapping.
+     */
     public fun <T> family(
         column: ColumnReference<T>,
     ): NonPositionalMapping<T, FontFamily> {
@@ -29,6 +47,12 @@ public interface WithFamily : BindingContext {
         )
     }
 
+    /**
+     * Maps the `family` aesthetic to a data column by [KProperty].
+     *
+     * @param column the data column to be mapped.
+     * @return a [NonPositionalMapping] object representing the mapping.
+     */
     public fun <T> family(
         column: KProperty<T>,
     ): NonPositionalMapping<T, FontFamily> {
@@ -39,6 +63,12 @@ public interface WithFamily : BindingContext {
         )
     }
 
+    /**
+     * Maps the `family` aesthetic to a data column by [String].
+     *
+     * @param column the data column to be mapped.
+     * @return a [NonPositionalMapping] object representing the mapping.
+     */
     public fun family(
         column: String,
     ): NonPositionalMapping<Any?, FontFamily> {
@@ -49,7 +79,13 @@ public interface WithFamily : BindingContext {
         )
     }
 
-    // Iterable, Array, PrimArray, DataColumn,
+    /**
+     * Maps the `family` aesthetic to the iterable of values.
+     *
+     * @param values the iterable of values to be mapped.
+     * @param name optional name for this aesthetic mapping.
+     * @return a [NonPositionalMapping] object representing the mapping.
+     */
     public fun <T> family(
         values: Iterable<T>,
         name: String? = null,
@@ -62,9 +98,14 @@ public interface WithFamily : BindingContext {
         )
     }
 
+    /**
+     * Maps the `family` aesthetic to a data column.
+     *
+     * @param values the data column to be mapped.
+     * @return a [NonPositionalMapping] object representing the mapping.
+     */
     public fun <T> family(
         values: DataColumn<T>,
-        //name: String? = null,
     ): NonPositionalMapping<T, FontFamily> {
         return addNonPositionalMapping<T, FontFamily>(
             FONT_FAMILY,
