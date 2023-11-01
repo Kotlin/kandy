fc4369d87491c4ed05ebcccd7eed92d37e717472
devcrocod <devcrocod@gmail.com>
2023-11-01 18:09:32 +0100

    Add kdoc for slope

--- kandy-lets-plot/src/main/kotlin/org/jetbrains/kotlinx/kandy/letsplot/layers/context/aes/WithSlope.kt
+++ kandy-lets-plot/src/main/kotlin/org/jetbrains/kotlinx/kandy/letsplot/layers/context/aes/WithSlope.kt
@@ -11,46 +11,85 @@ import org.jetbrains.kotlinx.kandy.ir.bindings.PositionalMapping
 import org.jetbrains.kotlinx.kandy.letsplot.internal.SLOPE
 import kotlin.reflect.KProperty
 
+/**
+ * Interface for managing the `slope` aesthetic.
+ *
+ * The `slope` aesthetic is used primarily in abLine plots to control the slope of the line,
+ * allowing for the representation of linear models or trend lines within the plot.
+ * A positive slope value results in a line angled upwards from left to right,
+ * while a negative value results in a line angled downwards from left to right.
+ * A slope of zero results in a horizontal line.
+ *
+ * Implementing this interface allows you to bind the `slope` aesthetic to a data column,
+ * enabling dynamic creation of lines based on your dataset.
+ */
 public interface WithSlope : BindingContext {
+
+    /**
+     * Provides a constant setter for the `slope` value.
+     *
+     * @property slope A [ConstantSetter] object to directly set the slope.
+     */
     public val slope: ConstantSetter
         get() = ConstantSetter(SLOPE, bindingCollector)
 
+    /**
+     * Maps the `slope` aesthetic to a data column by [ColumnReference].
+     *
+     * @param column the data column to be mapped.
+     * @return a [PositionalMapping] object representing the mapping.
+     */
     public fun <T> slope(
         column: ColumnReference<T>,
     ): PositionalMapping<T> {
         return addPositionalMapping<T>(SLOPE, column.name(), null)
     }
 
+    /**
+     * Maps the `slope` aesthetic to a data column by [KProperty].
+     *
+     * @param column the data column to be mapped.
+     * @return a [PositionalMapping] object representing the mapping.
+     */
     public fun <T> slope(
         column: KProperty<T>,
     ): PositionalMapping<T> {
         return addPositionalMapping<T>(SLOPE, column.name, null)
     }
 
+    /**
+     * Maps the `slope` aesthetic to a data column by [String].
+     *
+     * @param column the data column to be mapped.
+     * @return a [PositionalMapping] object representing the mapping.
+     */
     public fun slope(
         column: String,
     ): PositionalMapping<Any?> {
         return addPositionalMapping<Any?>(SLOPE, column, null)
     }
 
+    /**
+     * Maps the `slope` aesthetic to iterable of values.
+     *
+     * @param values The iterable of values to be mapped.
+     * @return A [PositionalMapping] object representing the mapping.
+     */
     public fun <T> slope(
         values: Iterable<T>,
     ): PositionalMapping<T> {
-        return addPositionalMapping<T>(
-            SLOPE,
-            values.toList(),
-            null,
-            null
-        )
+        return addPositionalMapping<T>(SLOPE, values.toList(), null, null)
     }
 
+    /**
+     * Maps the `slope` aesthetic to a data column.
+     *
+     * @param values the data column to be mapped.
+     * @return a [PositionalMapping] object representing the mapping.
+     */
     public fun <T> slope(
         values: DataColumn<T>,
     ): PositionalMapping<T> {
-        return addPositionalMapping<T>(
-            SLOPE,
-            values,
-            null
-        )
+        return addPositionalMapping<T>(SLOPE, values, null)
     }
 }
