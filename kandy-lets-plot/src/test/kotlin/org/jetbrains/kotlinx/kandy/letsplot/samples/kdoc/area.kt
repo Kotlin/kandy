a47e4b8e81c09f1de16d03026225b26e90b49ff8
devcrocod <devcrocod@gmail.com>
2023-11-02 00:11:15 +0100

    Add kdoc and test for area

---
+++ kandy-lets-plot/src/test/kotlin/org/jetbrains/kotlinx/kandy/letsplot/samples/kdoc/area.kt
@@ -0,0 +1,48 @@
+package org.jetbrains.kotlinx.kandy.letsplot.samples.kdoc
+
+import org.intellij.lang.annotations.Language
+import org.jetbrains.kotlinx.kandy.dsl.continuous
+import org.jetbrains.kotlinx.kandy.dsl.plot
+import org.jetbrains.kotlinx.kandy.letsplot.export.save
+import org.jetbrains.kotlinx.kandy.letsplot.jupyter.KandyLetsPlotJupyterTest
+import org.jetbrains.kotlinx.kandy.letsplot.layers.area
+import org.jetbrains.kotlinx.kandy.util.color.Color
+import org.jetbrains.kotlinx.kandy.util.context.invoke
+import kotlin.test.Test
+
+class AreaTest : KandyLetsPlotJupyterTest() {
+
+    @Language("kts")
+    private val plotArea = """
+        plot {
+            area {
+                // Positional mapping
+                x(listOf("January", "February", "March", "April", "May")) {
+                    axis.name = "months"
+                }
+                y(listOf(200, 150, 300, 250, 420)) {
+                    axis.name = "sales"
+                    scale = continuous(min = 100, max = 500)
+                }
+
+                // Non-positional settings
+                alpha = 0.5
+
+                // Non-positional mapping with constant fillColor
+                fillColor = Color.BLUE
+
+                // BorderLine settings
+                borderLine {
+                    color = Color.BLACK
+                    width = 1.5
+                }
+            }
+        }
+        """.trimIndent()
+
+    @Test
+    fun `compilation of area in jupyter`() = plotArea.checkCompilation()
+
+    @Test
+    fun `area output in jupyter`() = assertOutput(exec(plotArea))
+}
