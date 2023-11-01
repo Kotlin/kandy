fcb18eca4f0d8c2cbcf30fb743c12750a5c5a939
devcrocod <devcrocod@gmail.com>
2023-11-01 18:09:20 +0100

    Add kdoc for size

--- kandy-lets-plot/src/main/kotlin/org/jetbrains/kotlinx/kandy/letsplot/layers/context/aes/WithWidthAsSize.kt
+++ kandy-lets-plot/src/main/kotlin/org/jetbrains/kotlinx/kandy/letsplot/layers/context/aes/WithWidthAsSize.kt
@@ -12,13 +12,35 @@ import org.jetbrains.kotlinx.kandy.letsplot.internal.LetsPlotNonPositionalMappin
 import org.jetbrains.kotlinx.kandy.letsplot.internal.SIZE
 import kotlin.reflect.KProperty
 
+/**
+ * Interface for managing the `width` (size) aesthetic.
+ *
+ * The `width` aesthetic controls the size of individual elements within the plot, such as lines in a line plot.
+ * Depending on the plot type, the interpretation of `size` might differ,
+ * but generally, it is used to encode numerical values through visual size variations.
+ *
+ * This interface allows you to specify the label as a constant, map it to a column, or provide iterable of values.
+ */
 public interface WithWidthAsSize : BindingContext {
+
+    /**
+     * Sets a constant `width` for the layer.
+     *
+     * @property width the value to be set.
+     */
     public var width: Double?
         get() = null
         set(value) {
             addNonPositionalSetting(SIZE, value)
         }
 
+    /**
+     * Maps the `width` aesthetic to a data column by [ColumnReference].
+     *
+     * @param column the data column to map to the size.
+     * @param parameters optional lambda to configure additional scale parameters.
+     * @return a [NonPositionalMapping] object representing the mapping.
+     */
     public fun <T> width(
         column: ColumnReference<T>,
         parameters: LetsPlotNonPositionalMappingParametersContinuous<T, Double>.() -> Unit = {}
@@ -30,6 +52,13 @@ public interface WithWidthAsSize : BindingContext {
         )
     }
 
+    /**
+     * Maps the `width` aesthetic to a data column by [KProperty].
+     *
+     * @param column the data column to map to the size.
+     * @param parameters optional lambda to configure additional scale parameters.
+     * @return a [NonPositionalMapping] object representing the mapping.
+     */
     public fun <T> width(
         column: KProperty<T>,
         parameters: LetsPlotNonPositionalMappingParametersContinuous<T, Double>.() -> Unit = {}
@@ -41,6 +70,13 @@ public interface WithWidthAsSize : BindingContext {
         )
     }
 
+    /**
+     * Maps the `width` aesthetic to a data column by [String].
+     *
+     * @param column the data column to map to the size.
+     * @param parameters optional lambda to configure additional scale parameters.
+     * @return a [NonPositionalMapping] object representing the mapping.
+     */
     public fun width(
         column: String,
         parameters: LetsPlotNonPositionalMappingParametersContinuous<Any?, Double>.() -> Unit = {}
@@ -52,7 +88,14 @@ public interface WithWidthAsSize : BindingContext {
         )
     }
 
-    // Iterable, Array, PrimArray, DataColumn,
+    /**
+     * Maps the `width` aesthetic to iterable of discrete values.
+     *
+     * @param values the iterable containing the discrete values.
+     * @param name optional name for this aesthetic mapping.
+     * @param parameters optional lambda to configure additional scale parameters.
+     * @return a [NonPositionalMapping] object representing the mapping.
+     */
     public fun <T> width(
         values: Iterable<T>,
         name: String? = null,
@@ -66,6 +109,13 @@ public interface WithWidthAsSize : BindingContext {
         )
     }
 
+    /**
+     * Maps the `width` aesthetic to a data column.
+     *
+     * @param values the data column to map to the size.
+     * @param parameters optional lambda to configure additional scale parameters.
+     * @return a [NonPositionalMapping] object representing the mapping.
+     */
     public fun <T> width(
         values: DataColumn<T>,
         parameters: LetsPlotNonPositionalMappingParametersContinuous<T, Double>.() -> Unit = {}
