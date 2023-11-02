2a6bb979a86ed6d707742afc007a2f48f0842e34
devcrocod <devcrocod@gmail.com>
2023-11-02 23:23:26 +0100

    Add kdoc and test for raster

--- kandy-lets-plot/src/main/kotlin/org/jetbrains/kotlinx/kandy/letsplot/layers/context/Raster.kt
+++ kandy-lets-plot/src/main/kotlin/org/jetbrains/kotlinx/kandy/letsplot/layers/context/Raster.kt
@@ -16,12 +16,42 @@ import org.jetbrains.kotlinx.kandy.letsplot.layers.context.aes.WithX
 import org.jetbrains.kotlinx.kandy.letsplot.layers.context.aes.WithY
 import org.jetbrains.kotlinx.kandy.letsplot.layers.geom.RASTER
 
-public interface RasterInterface: LayerContextInterface, WithBorderLine, WithX, WithY,
+/**
+ * Interface defining the necessary aesthetics and methods for raster layers.
+ *
+ * Raster layers are used to represent data in a grid format,
+ * commonly used for heatmaps or geographic information systems.
+ * The interface provides aesthetics such as `x`, `y`, `fillColor`,
+ * and `alpha` for a complete customization of raster representation.
+ *
+ * Required aesthetics for raster layers are `x` and `y`.
+ */
+public interface RasterInterface : LayerContextInterface, WithX, WithY,
     WithFillColor, WithAlpha {
+
+    /**
+     * Gets the Geom object specific to **raster** layers.
+     *
+     * @return the [Geom] object for **raster**.
+     */
     override val geom: Geom
         get() = RASTER
+
+    /**
+     * Gets the set of required aesthetics for **raster** layers.
+     *
+     * @return the set of required aesthetics.
+     */
     override val requiredAes: Set<Aes>
         get() = setOf(X, Y)
 }
 
+/**
+ * Context class for managing raster layers.
+ *
+ * This class provides the context in which raster layers can be configured.
+ * It inherits from [LayerWithBorderLineContext] and implements the [RasterInterface].
+ *
+ * @param parent the parent context for the layer.
+ */
 public open class RasterContext(parent: LayerCollectorContext) : LayerWithBorderLineContext(parent), RasterInterface
