24bd15126e7f8292704a4b6ef24f371c0e76f112
devcrocod <devcrocod@gmail.com>
2023-11-02 18:59:36 +0100

    Add kdoc and test for hline, vline

--- kandy-lets-plot/src/main/kotlin/org/jetbrains/kotlinx/kandy/letsplot/layers/context/VLine.kt
+++ kandy-lets-plot/src/main/kotlin/org/jetbrains/kotlinx/kandy/letsplot/layers/context/VLine.kt
@@ -13,14 +13,41 @@ import org.jetbrains.kotlinx.kandy.letsplot.internal.X_INTERCEPT
 import org.jetbrains.kotlinx.kandy.letsplot.layers.context.aes.*
 import org.jetbrains.kotlinx.kandy.letsplot.layers.geom.V_LINE
 
-public interface VLineInterface: LayerContextInterface, WithXIntercept, WithAlpha, WithColor,
+/**
+ * Interface for defining the necessary aesthetics and methods for vertical line layers.
+ *
+ * Vertical line layers are useful for indicating specific values along the X-axis.
+ * This interface provides the required aesthetics like `xIntercept`,
+ * `alpha`, `color`, `width`, and `type` for customization.
+ *
+ * The set of required aesthetics for vertical line layers includes `X_INTERCEPT`.
+ */
+public interface VLineInterface : LayerContextInterface, WithXIntercept, WithAlpha, WithColor,
     WithWidthAsSize, WithType, WithYFree {
+
+    /**
+     * Gets the Geom object specific to **vLine** layers.
+     *
+     * @return the [Geom] object for **vLine**.
+     */
     override val geom: Geom
         get() = V_LINE
+
+    /**
+     * Gets the set of required aesthetics for **vLine** layers.
+     *
+     * @return the set of required aesthetics.
+     */
     override val requiredAes: Set<Aes>
         get() = setOf(X_INTERCEPT)
 }
 
-public open class VLineContext(parent: LayerCollectorContext) : LayerContext(parent), VLineInterface {
-    override val requiredAes: Set<Aes> = setOf(X_INTERCEPT)
-    }
+/**
+ * Context class for managing vertical line layers.
+ *
+ * This class serves as the context in which vertical line layers can be customized.
+ * It inherits from [LayerContext] and implements the [VLineInterface].
+ *
+ * @param parent the parent context for the layer.
+ */
+public open class VLineContext(parent: LayerCollectorContext) : LayerContext(parent), VLineInterface
