a247abb6ac7c11fdb302efa2b95a8ddca2c7c4f7
Andrei <kisandy@yandex.ru>
2024-02-01 16:31:40 +0400

    fix grammar in whiskerWidth kdocs

--- kandy-lets-plot/src/main/kotlin/org/jetbrains/kotlinx/kandy/letsplot/layers/context/aes/WithWhiskerWidth.kt
+++ kandy-lets-plot/src/main/kotlin/org/jetbrains/kotlinx/kandy/letsplot/layers/context/aes/WithWhiskerWidth.kt
@@ -9,15 +9,15 @@ import org.jetbrains.kotlinx.kandy.letsplot.internal.FATTEN
 import org.jetbrains.kotlinx.kandy.letsplot.internal.WHISKER_WIDTH
 
 /**
- * Interface for managing the `whiskerWidth` aesthetic, which is responsible for the length of boxplor whiskers.
+ * Interface for managing the `whiskerWidth` aesthetic, which is responsible for the length of boxplot whiskers.
  *
  * Implementing this interface allows the aesthetic to be directly set as a numeric value.
  */
 public interface WithWhiskerWidth : BindingContext {
 
     /**
-     * Sets the `whiskerWidth` aesthetic, determining the ratio of the length of box ends to the width of the box.
-     * This aesthetic is specifically applicable in boxes-based plots (boxplot, candlestick, etc.).
+     * Sets the whiskerWidth aesthetic, determining the ratio of the whisker ends width to the box's width.
+     * This aesthetic specifically applies to boxes-based plots (boxplot, candlestick, etc.)
      *
      * @property whiskerWidth a numeric value that represents the ratio of the length of box ends to the width of the box.
      */
