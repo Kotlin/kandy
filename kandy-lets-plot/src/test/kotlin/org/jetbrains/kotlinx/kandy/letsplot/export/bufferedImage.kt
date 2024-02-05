c3f9a32740e59d69768f8461f9026e57a3372984
Andrei <kisandy@yandex.ru>
2024-02-05 17:27:23 +0400

    export tests

--- kandy-lets-plot/src/test/kotlin/org/jetbrains/kotlinx/kandy/letsplot/export/bufferedImage.kt
+++ kandy-lets-plot/src/test/kotlin/org/jetbrains/kotlinx/kandy/letsplot/export/bufferedImage.kt
@@ -3,6 +3,8 @@ package org.jetbrains.kotlinx.kandy.letsplot.export
 import org.jetbrains.kotlinx.kandy.dsl.plot
 import org.jetbrains.kotlinx.kandy.ir.Plot
 import org.jetbrains.kotlinx.kandy.letsplot.layers.line
+import org.jetbrains.kotlinx.kandy.letsplot.multiplot.plotBunch
+import org.jetbrains.kotlinx.kandy.letsplot.multiplot.plotGrid
 import org.junit.Assert.assertNotEquals
 import kotlin.test.Test
 import kotlin.test.assertNotNull
@@ -19,6 +21,42 @@ class BufferedImageTest {
         assertNotNull(bufferedImage)
 
         val bufferedImageWithDifferentScale = plot.toBufferedImage(1.0, dpi)
+        assertNotEquals(bufferedImage.width, bufferedImageWithDifferentScale.width)
+        assertNotEquals(bufferedImage.height, bufferedImageWithDifferentScale.height)
+    }
+
+    @Test
+    fun `export plot grid as BufferedImage`() {
+        val plot: Plot = plot { line { x(listOf(1, 2, 3)); y(listOf(0.3, 0.15, 0.53)) } }
+        val grid = plotGrid(listOf(plot, plot, plot))
+        val scale = 2.0
+        val dpi = 300
+
+        val bufferedImage = grid.toBufferedImage(scale, dpi)
+
+        assertNotNull(bufferedImage)
+
+        val bufferedImageWithDifferentScale = grid.toBufferedImage(1.0, dpi)
+        assertNotEquals(bufferedImage.width, bufferedImageWithDifferentScale.width)
+        assertNotEquals(bufferedImage.height, bufferedImageWithDifferentScale.height)
+    }
+
+    @Test
+    fun `export plot bunch as BufferedImage`() {
+        val plot: Plot = plot { line { x(listOf(1, 2, 3)); y(listOf(0.3, 0.15, 0.53)) } }
+        val bunch = plotBunch {
+            add(plot, 0, 0)
+            add(plot, 400, 600)
+            add(plot, 800, 600)
+        }
+        val scale = 2.0
+        val dpi = 300
+
+        val bufferedImage = bunch.toBufferedImage(scale, dpi)
+
+        assertNotNull(bufferedImage)
+
+        val bufferedImageWithDifferentScale = bunch.toBufferedImage(1.0, dpi)
         assertNotEquals(bufferedImage.width, bufferedImageWithDifferentScale.width)
         assertNotEquals(bufferedImage.height, bufferedImageWithDifferentScale.height)
     }
