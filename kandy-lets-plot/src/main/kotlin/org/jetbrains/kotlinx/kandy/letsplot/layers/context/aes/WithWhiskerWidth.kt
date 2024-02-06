04e081ed37bf45756da5f684c2472ccfc79e8feb
Andrei <kisandy@yandex.ru>
2024-02-06 18:19:46 +0400

    fix whiskerWidth doc

--- kandy-lets-plot/src/main/kotlin/org/jetbrains/kotlinx/kandy/letsplot/layers/context/aes/WithWhiskerWidth.kt
+++ kandy-lets-plot/src/main/kotlin/org/jetbrains/kotlinx/kandy/letsplot/layers/context/aes/WithWhiskerWidth.kt
@@ -9,7 +9,7 @@ import org.jetbrains.kotlinx.kandy.letsplot.internal.FATTEN
 import org.jetbrains.kotlinx.kandy.letsplot.internal.WHISKER_WIDTH
 
 /**
- * Interface for managing the `whiskerWidth` aesthetic, which is responsible for the length of boxplot whiskers.
+ * Interface for managing the `whiskerWidth` aesthetic, which is responsible for the length of boxplot whiskers ends.
  *
  * Implementing this interface allows the aesthetic to be directly set as a numeric value.
  */
