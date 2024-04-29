f6521533ec042c9ef03166523209576b1e2cde9d
Andrei <kisandy@yandex.ru>
2024-04-29 19:51:32 +0400

    grouped data comment

--- kandy-api/src/main/kotlin/org/jetbrains/kotlinx/kandy/dsl/internal/GroupedDataScope.kt
+++ kandy-api/src/main/kotlin/org/jetbrains/kotlinx/kandy/dsl/internal/GroupedDataScope.kt
@@ -3,7 +3,8 @@ package org.jetbrains.kotlinx.kandy.dsl.internal
 import org.jetbrains.kotlinx.dataframe.columns.ColumnGroup
 
 /// todo docs
-// must be LayerCreatorScope, marker for containing grouped data
+// must be LayerCreatorScope, marker of containing grouped data
+// (needed for transformations, especially statistics)
 public sealed interface GroupedDataScope<T> {
     public val key: ColumnGroup<T>
 }
