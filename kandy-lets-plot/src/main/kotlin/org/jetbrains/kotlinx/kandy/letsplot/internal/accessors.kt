54f7ec0ca0e877164fdac4dac8a202e48862cdba
Andrei <kisandy@yandex.ru>
2024-07-22 14:38:01 +0400

    fix duplicate

--- kandy-lets-plot/src/main/kotlin/org/jetbrains/kotlinx/kandy/letsplot/internal/accessors.kt
+++ kandy-lets-plot/src/main/kotlin/org/jetbrains/kotlinx/kandy/letsplot/internal/accessors.kt
@@ -7,12 +7,10 @@ import org.jetbrains.kotlinx.kandy.dsl.internal.PlotBuilder
 import org.jetbrains.kotlinx.kandy.dsl.internal.dataframe.DatasetBuilderImpl
 import org.jetbrains.kotlinx.kandy.dsl.internal.datasetBuilder
 
-@Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
 @PublishedApi
 internal val LayerBuilder.datasetBuilderImpl: DatasetBuilderImpl
     get() = datasetBuilder as DatasetBuilderImpl
 
-@Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
 @PublishedApi
 internal val PlotBuilder.datasetBuilderImpl: DatasetBuilderImpl
     get() = datasetBuilder as DatasetBuilderImpl
